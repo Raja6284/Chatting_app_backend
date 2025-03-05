10381b57b4013503ceb2db7e458ff0c5f17dd6b7
Raja kumar <rajaraja360parth@gmail.com>
2025-03-05 21:11:03 +0530

    broadcass message with room id logic

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,52 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer,WebSocket } from "ws";
 
 //creates a WebSocket server instance that listen to WebSocket connections on port:8080
 const wss = new WebSocketServer({port:8080})
 
-let userCount = 0
-let allSockets = []
+interface User{
+    socket:WebSocket,
+    roomId:string
+}
+let allSockets:User[] = []
+let useCount = 0
 
 //event handler
 wss.on('connection',(socket)=>{
     
-    allSockets.push(socket)
-    userCount = userCount + 1
-    console.log("userCount#" + userCount)
-
     socket.on('message',(message)=>{
-        console.log("message received : " + message.toString())
-        // setTimeout(()=>{
-        //     socket.send(message.toString() + " from the server")
-        // },2000)
-
-        for(let i = 0; i<allSockets.length; i++){
-            let c = allSockets[i];
-            c.send(message.toString() + " from the server")
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message)//same as ts ignore
+       
+        if(parsedMessage.type == "join"){
+            allSockets.push({
+                socket:socket,
+                roomId:parsedMessage.payload.roomId
+            })
+            console.log("user joined the room : " + parsedMessage.payload.roomId)
+            useCount = useCount + 1
+            console.log("userCoutn#"+useCount)
         }
+
+
+        if(parsedMessage.type == "chat"){
+            //console.log("user wants to chat")
+            let currentUserRoom = null
+            for(let i = 0; i<allSockets.length; i++){
+                if(allSockets[i].socket == socket){
+                    currentUserRoom = allSockets[i].roomId
+                }
+            }
+
+            for(let i = 0; i<allSockets.length; i++){
+                if(allSockets[i].roomId == currentUserRoom){
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }   
+        }
+
     })
+
+    // socket.on("close",()=>{
+    //     allSockets = allSockets.filter(x => x != socket)
+    // })
 })
